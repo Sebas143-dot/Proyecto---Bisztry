e5b95fa15bcdbea31ff6131809567980972ec714
Juan David Ipiales <jdipialesi@utn.edu.ec>
2025-07-22 11:37:37 -0500

    a5

--- Proyecto-Bisztry/config/database.php
+++ Proyecto-Bisztry/config/database.php
@@ -64,18 +64,22 @@ return [
         ],
 
 'pgsql' => [
-    'driver' => 'pgsql',
-    'host' => env('DB_HOST', env('PGHOST', '127.0.0.1')),
-    'port' => env('DB_PORT', env('PGPORT', '5432')),
-    'database' => env('DB_DATABASE', env('PGDATABASE', 'forge')),
-    'username' => env('DB_USERNAME', env('PGUSER', 'forge')),
-    'password' => env('DB_PASSWORD', env('PGPASSWORD', '')),
-    'charset' => 'utf8',
-    'prefix' => '',
-    'prefix_indexes' => true,
-    'search_path' => 'public',
-    'sslmode' => env('DB_SSLMODE', 'prefer'),
-],
+            'driver' => 'pgsql',
+            // --- INICIO DE LA CORRECCIÓN ---
+            // Le decimos a Laravel que use la variable de entorno DB_HOST. Si no existe,
+            // entonces que use la variable PGHOST que nos da Railway. Hacemos lo mismo para las demás.
+            'host' => env('DB_HOST', env('PGHOST', '127.0.0.1')),
+            'port' => env('DB_PORT', env('PGPORT', '5432')),
+            'database' => env('DB_DATABASE', env('PGDATABASE', 'forge')),
+            'username' => env('DB_USERNAME', env('PGUSER', 'forge')),
+            'password' => env('DB_PASSWORD', env('PGPASSWORD', '')),
+            // --- FIN DE LA CORRECCIÓN ---
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
