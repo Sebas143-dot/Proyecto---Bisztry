b1a1d41abae157682e292ac5b05da2d6f01485b7
Juan David Ipiales <jdipialesi@utn.edu.ec>
2025-07-22 18:49:55 -0500

    Corregido rutas admin

--- Proyecto-Bisztry/app/Http/Kernel.php
+++ Proyecto-Bisztry/app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-'role' => \App\Http\Middleware\DebugRoleMiddleware::class, // Usamos nuestro "micrófono"
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         // En app/Http/Kernel.php, dentro de protected $middlewareAliases
     'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
--- Proyecto-Bisztry/app/Http/Middleware/CheckRole.php
+++ Proyecto-Bisztry/app/Http/Middleware/CheckRole.php
@@ -5,35 +5,22 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 
 class CheckRole
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string  $role // Aquí recibiremos el rol (ej: 'superadmin')
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next, $role)
+    public function handle(Request $request, Closure $next, ...$roles)
     {
-        // Primero, verifica si el usuario está autenticado.
-        if (!Auth::check()) {
-            return redirect('login');
+        if (Auth::guest()) {
+            throw UnauthorizedException::notLoggedIn();
         }
 
-        // Asumiremos que 'superadmin' significa que la columna 'is_admin' es verdadera.
-        // Puedes ajustar esta lógica según cómo gestiones los roles.
-        if ($role === 'superadmin' && $request->user()->is_admin) {
+        $user = Auth::user();
+
+        if ($user->hasAnyRole($roles)) {
             return $next($request);
         }
 
-        // Si el usuario no tiene el rol requerido, redirígelo o muestra un error.
-        // Por ejemplo, puedes redirigirlo a la página de inicio.
-        return redirect('/')->with('error', 'No tienes permisos para acceder a esta sección.');
-
-        // O, si prefieres, puedes mostrar una página de error 403 (Prohibido).
-        // abort(403, 'Acceso no autorizado.');
+        throw UnauthorizedException::forRoles($roles);
     }
 }
