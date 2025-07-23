f382cba4f4ff8c7d0eac5dfbd481071f8c10865c
Juan David Ipiales <jdipialesi@utn.edu.ec>
2025-07-22 19:14:37 -0500

    a

--- Proyecto-Bisztry/resources/views/users/index.blade.php
+++ Proyecto-Bisztry/resources/views/users/index.blade.php
@@ -3,9 +3,27 @@
 @section('title', 'Gestión de Usuarios')
 @section('page-title', 'Usuarios del Sistema')
 @section('page-description', 'Administra las cuentas de usuario y sus roles.')
+{{-- Bloque para mostrar mensajes de éxito (AHORA CON BOTÓN DE CIERRE) --}}
 @if (session('success'))
-    <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
-        <p>{{ session('success') }}</p>
+    <div x-data="{ show: true }" 
+         x-init="setTimeout(() => show = false, 5000)" 
+         x-show="show" 
+         x-transition:enter="transition ease-out duration-300"
+         x-transition:enter-start="opacity-0 transform translate-y-2"
+         x-transition:enter-end="opacity-100 transform translate-y-0"
+         x-transition:leave="transition ease-in duration-300"
+         x-transition:leave-start="opacity-100 transform translate-y-0"
+         x-transition:leave-end="opacity-0 transform translate-y-2"
+         class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4 flex justify-between items-center shadow-md rounded-lg" 
+         role="alert">
+
+        <p class="font-medium">{{ session('success') }}</p>
+
+        <button @click="show = false" class="text-green-500 hover:text-green-700 focus:outline-none">
+            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            </svg>
+        </button>
     </div>
 @endif
 
